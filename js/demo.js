06361a5973ad71930244915ec4a8f5fe2593a250
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-08-30 16:06:23 +0300

    Refactor, add the element first and showing only on delayed loop

--- js/demo.js
+++ js/demo.js
@@ -22,47 +22,48 @@ window.onload = function() {
 
 function character( img, text ) {
 	
-	// start movein with a few vibras
+	// container for the avatar
+	var div = $('<div>', { 'class': 'sam' });
+	
+	// content
+	var i = $('<img>', { src: img, height: 175 } );
+	div.append( i );
+	
+	if( text ) {
+		
+		var span = $('<span>', { html: text } );
+		span.css('background', 'black');
+		span.css('padding', 3 );
+		
+		
+		div.append( '<br/>' );
+		div.append( span );
+	
+	};
+	
+	// show element
+	div.hide();
+	$('body').prepend( div );
+	
+	
+	// hide the element after click
+	div.on('click', function( e ) {
+		
+		div.hide('slide', 'fast', function() {
+			div.remove();
+		});
+		
+		e.stopPropagation();
+	});
+	
+	// show the avatar here
+	
+	// start move in with a few vibras
 	
 	navigator.vibrate([50, 100, 50]);
 	
-	// animate first movein
 	setTimeout( function() {
-		
-		var div = $('<div>', { 'class': 'sam' });
-		
-		// content
-		var i = $('<img>', { src: img, height: 175 } );
-		div.append( i );
-		
-		if( text ) {
-			
-			var span = $('<span>', { html: text } );
-			span.css('background', 'black');
-			span.css('padding', 3 );
-			
-			
-			div.append( '<br/>' );
-			div.append( span );
-		
-		};
-		
-		// show element
-		div.hide();
-		$('body').prepend( div );
 		div.show('slide', 'slow');
-		
-		
-		// hide the element after click
-		div.on('click', function( e ) {
-			
-			div.hide('slide', 'fast', function() {
-				div.remove();
-			});
-			
-			e.stopPropagation();
-		});
-		
 	}, 1750 );
 	
 }
