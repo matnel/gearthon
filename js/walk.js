e6cc428780ae9077c8f3f0934b3035ed7b5b78f0
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-08-30 10:57:48 +0300

    Motion API added

--- js/walk.js
+++ js/walk.js
@@ -1,12 +1,17 @@
 window.onload = function() {
 	
-	var sensor = window.webapis.motion;
+	/** var sensor = window.webapis.motion;
 	
 	sensor.start( 'PEDOMETER',
 		function onSuccess( data ) {
 			console.log( data );
 		}
-	);
-
+	); **/
+	
+	
+	window.ondevicemotion = function( data ) {
+		console.log(data.acceleration.x);
+	}
 	
 }
+
