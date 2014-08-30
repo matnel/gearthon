4a72bc16c9bc49a815c50c43fe88ad5e19167211
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-08-30 09:42:05 +0300

    Get the security stuff right

--- js/walk.js
+++ js/walk.js
@@ -1,21 +1,12 @@
 window.onload = function() {
 	
-	
-	
-	
-	
-	
-	
-	// load and initialize
-	
-	console.log("let's load sensors");
 	var sensor = window.webapis.motion;
 	
 	sensor.start( 'PEDOMETER',
-			function onSuccess( data ) {
-				console.log( data );
-            }
-        );
+		function onSuccess( data ) {
+			console.log( data );
+		}
+	);
 
 	
 }
